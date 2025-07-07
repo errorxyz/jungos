8a54d8139ffac70d4115e8928f93c5734b6add28
errorxyz <gjain1563@gmail.com>
2025-07-07 10:12:00 +0530

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+// dont link std lib
 #![no_std]
+
 #![no_main]
 use core::panic::PanicInfo;
 
@@ -7,6 +9,8 @@ pub extern "C" fn _start() -> !{
     loop {}
 }
 
+// since we're not linking std lib, we need to define a panic handler
+// Return type is ! signifying the "never" return type
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
